45db36a8d1c982f423af9f2af412913bb99d22ae
Maksim_Chervyakov <pesinen88@gmail.com>
2019-05-24 08:18:31 +0600

    Исправлены критерии: Б9, Б13, Б30, Б31

--- source/js/app.js
+++ source/js/app.js
@@ -1,37 +1,37 @@
-var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.page-header__toggle');
-  var header = document.querySelector('.page-header');
-  var contactsMap = document.querySelector('.contacts__map');
+var navMain = document.querySelector(".main-nav");
+  var navToggle = document.querySelector(".page-header__toggle");
+  var header = document.querySelector(".page-header");
+  var contactsMap = document.querySelector(".contacts__map");
 
-  contactsMap.classList.remove('contacts__map--no-js');
-  header.classList.remove('page-header--nojs');
-  navMain.classList.add('main-nav--closed');
+  contactsMap.classList.remove("contacts__map--no-js");
+  header.classList.remove("page-header--nojs");
+  navMain.classList.add("main-nav--closed");
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navToggle.classList.remove('page-header__toggle--closed');
-      navToggle.classList.add('page-header__toggle--opened');
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navToggle.classList.remove("page-header__toggle--closed");
+      navToggle.classList.add("page-header__toggle--opened");
     } else {
-      navMain.classList.add('main-nav--closed');
-      navToggle.classList.add('page-header__toggle--closed');
-      navToggle.classList.remove('page-header__toggle--opened');
+      navMain.classList.add("main-nav--closed");
+      navToggle.classList.add("page-header__toggle--closed");
+      navToggle.classList.remove("page-header__toggle--opened");
     }
   });
 
   ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
+    var myMap = new ymaps.Map("map", {
           center: [59.938791, 30.322560],
           zoom: 18
         }, {
-          searchControlProvider: 'yandex#search'
+          searchControlProvider: "yandex#search"
         }),
 
         myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-          hintContent: 'Cat-energy'
+          hintContent: "Cat-energy"
         }, {
-          iconLayout: 'default#image',
-          iconImageHref: '../img/map-pin.png',
+          iconLayout: "default#image",
+          iconImageHref: "../img/map-pin.png",
           iconImageSize: [124, 106],
           iconImageOffset: [-50, -100]
         });
